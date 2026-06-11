9f610d8ea5a94b50a79a18427bae52ffd739974a
surojit-ghosh <contact@surojit.in>
2026-06-11 10:03:41 +0530

    add: june 11

--- cap_06_10/react-app/src/components/Child4.jsx
+++ cap_06_10/react-app/src/components/Child4.jsx
@@ -1,9 +1,9 @@
 const Child4 = ({ theme }) => {
   return (
     <div
-      className={`p-4 ${theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-gray-800"}`}
+      className={`p-4 text-center ${theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-gray-800"}`}
     >
-      <h1>This is the most inner Child</h1>
+      <h1 className="text-xl font-bold">This is the most inner Child</h1>
     </div>
   );
 };
